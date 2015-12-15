83f18a4304ea3388da9e6d64c75b79e9308a7a6b
yanir manor <caldigitalteam@gmail.com>
2015-12-15 10:30:49 +0200

    update webpack for scss autoprefixer

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+// Import CSS
+import './src/css/master.scss'
 
 ReactDOM.render(<App /> , document.getElementById('app') )
